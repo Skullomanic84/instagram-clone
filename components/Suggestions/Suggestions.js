b381cff493edd24ebf13b1decc265ee73af1ec3e
Jahmaine Shiloh <55574810+Skullomanic84@users.noreply.github.com>
2022-09-28 20:31:50 +0200

    Updated Suggestions.js
    imported and pulled dummy data from faker js. Created state management with useState and cleaned up with useEffect. Mapped through the suggestion array to fetch random username, avatar and company name.

--- components/Suggestions/Suggestions.js
+++ components/Suggestions/Suggestions.js
@@ -1,9 +1,51 @@
-
+import { useEffect, useState } from "react";
+import { faker } from '@faker-js/faker';
 
 function Suggestions() {
+
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    const suggestions = [...Array(10)].map((_, i) => (
+      {
+        userId: faker.datatype.uuid(),
+          username: faker.internet.userName(),
+          avatar: faker.image.avatar(),
+          company: faker.company.name(),
+      }
+    ));
+
+    setSuggestions(suggestions);
+  }, [])
+
   return (
-    <div>Suggestions</div>
-  )
+    <div className="mt-4 ml-10">
+     <div className="flex justify-between text-sm mb-5 ">
+      <h3 className="text-sm font-bold text-gray-400">Suggestion for you</h3>
+      <button className="text-gray-600 font-semibold">See  All</button>
+     </div>
+
+     {/* mapping through the suggestions array */}
+     {suggestions.map( profile => (
+      <div key={profile.id} className="flex items-center justify-between mt-3"> 
+        <picture>
+          <img 
+          src={profile.avatar} 
+          alt={profile.username}
+          className="w-10 h-10 rounded-full border p-[2px] " />
+        </picture>
+
+        <div className="flex-1 ml-4">
+          <h2 className='font-semibold text-sm'>{profile.username}</h2>
+          <h3 className="text-xs text-gray-400">Works at {profile.company}</h3>
+        </div>
+
+        <button className="text-blue-400 text-sm">Follow</button>
+      </div>
+     ))
+    }
+    </div>
+  );
 }
 
 export default Suggestions
